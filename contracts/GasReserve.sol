b6703297f826eaca960a25b71125731afffe636c
Emiliano A. Billi <emiliano.billi@gmail.com>
2020-01-26 18:43:31 -0300

    Create GasReserve.sol

---
+++ contracts/GasReserve.sol
@@ -0,0 +1,59 @@
+pragma solidity 0.5.12;
+
+
+contract GasReserve {
+    uint256[] g;
+    
+    function reserveGas(uint256 quantity) internal {
+        if (quantity > 0) 
+            reserve(quantity);
+    }
+    
+    function releaseGas(uint256 quantity) internal {
+        if (quantity > 0 && quantity <= g.length)
+            release(quantity);
+    }
+    
+    function gasLen() internal view returns (uint256) {
+        return g.length;
+    }
+    
+    function getReserveAddr () private pure returns(uint256 reserve) {
+        uint256 gaddr;
+        assembly {
+            gaddr := g_slot
+        }
+        return uint256(keccak256(abi.encode(gaddr)));
+    }
+    
+    function reserve(uint256 quantity) private {
+        uint256 start = getReserveAddr() + g.length;
+        uint256 end = start + quantity;
+        uint256 len = g.length + quantity;
+        
+        for (uint256 i = start; i < end; i ++) {
+            assembly {
+                sstore(i,1)
+            }
+        }
+        assembly {
+            sstore(g_slot, len)
+        }
+    }
+    
+    function release(uint256 quantity) private {
+        uint256 start = getReserveAddr() + (g.length - quantity);
+        uint256 end = getReserveAddr() + g.length;
+        uint256 len = g.length - quantity;
+        
+        for (uint256 i = start; i < end; i++) {
+            assembly {
+                sstore(i,0)
+            }
+        }
+        assembly {
+            sstore(g_slot, len)
+        }
+    }
+    
+}
